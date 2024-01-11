7893a659c326c837da88ef77dff66477f2d58236
bachenour <bache.nour2@gmail.com>
2024-01-11 21:08:44 +0100

    adding some logic

--- src/Controller/SalesOrderController.php
+++ src/Controller/SalesOrderController.php
@@ -3,17 +3,35 @@
 namespace App\Controller;
 
 use App\Entity\SalesOrder;
+use App\Entity\OrderProduct;
 use App\Repository\SalesOrderRepository;
 use App\Form\SalesOrderType;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CartRepository;
+use App\Repository\CartProductRepository;
+use App\Repository\ProductRepository;
+use App\Repository\OrderProductRepository;
 
 #[Route('/sales/order')]
 class SalesOrderController extends AbstractController
 {
+    private $cartRepository;
+    private $cartProductRepository;
+    private $orderProductRepo;
+
+    public function __construct(CartRepository $cartRepository, CartProductRepository $cartProductRepository, OrderProductRepository $orderProductRepo)
+    {
+        $this->cartRepository  = $cartRepository;
+        $this->cartProductRepository  = $cartProductRepository;
+        $this->orderProductRepo = $orderProductRepo;
+    }
+
+
     #[Route('/', name: 'app_sales_order_index', methods: ['GET'])]
     public function index(SalesOrderRepository $salesOrderRepository): Response
     {
@@ -22,31 +40,77 @@ class SalesOrderController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_sales_order_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/userorders', name: 'app_sales_order_user', methods: ['GET'])]
+    public function userOrders(SalesOrderRepository $salesOrderRepository): Response
     {
-        $salesOrder = new SalesOrder();
-        $form = $this->createForm(SalesOrderType::class, $salesOrder);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($salesOrder);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_sales_order_index', [], Response::HTTP_SEE_OTHER);
+        $user = $this->getUser();
+        
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
         }
-
-        return $this->render('sales_order/new.html.twig', [
-            'sales_order' => $salesOrder,
-            'form' => $form,
+        return $this->render('sales_order/index.html.twig', [
+            'sales_orders' => $salesOrderRepository->findBy(['user' => $user]),
         ]);
     }
 
-    #[Route('/{id}', name: 'app_sales_order_show', methods: ['GET'])]
-    public function show(SalesOrder $salesOrder): Response
+    #[Route('/new', name: 'app_sales_order_new', methods: ['GET', 'POST'])]
+    public function new(EntityManagerInterface $entityManager): Response
     {
+        $user = $this->getUser();
+
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        //create a new sales order
+        $salesOrder = new SalesOrder();
+        $salesOrder->setUser($user);
+        $salesOrder->setDateOrder(new DateTime());
+        // TODO add 7 days 
+        $salesOrder->setDateDelivery(new DateTime());
+        $entityManager->persist($salesOrder);
+        $entityManager->flush();
+
+        //create new lines in productOrder
+        $cart = $this->cartRepository->findOneBy(['user' => $user, 'saved' => false]);
+        if(!$cart){
+            return $this->redirectToRoute('home');
+        }
+        $cartProducts = $this->cartProductRepository->findBy(['cart' => $cart]);
+        foreach($cartProducts as $cartProduct){
+            $product = $cartProduct->getProduct();
+            $orderProduct = new OrderProduct();
+            $orderProduct->setProduct($product);
+            $orderProduct->setSalesOrder($salesOrder);
+            //TODO update with the real quantity
+            $orderProduct->setQuantity($cartProduct->getQuantity());
+            
+            $total = $cartProduct->getQuantity() * $product->getPrice();
+            $orderProduct->setTotal($total);
+
+            $entityManager->persist($orderProduct);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_sales_order_user', [], Response::HTTP_SEE_OTHER);
+    }
+
+    #[Route('/{id}', name: 'app_sales_order_show', methods: ['GET'])]
+    public function show(SalesOrder $salesOrder, EntityManagerInterface $entityManager): Response
+    {
+        $orderProductRepository = $entityManager->getRepository(OrderProduct::class);
+        $orderProducts = $orderProductRepository->findBy(['salesOrder' => $salesOrder]);
+
+        $products = [];
+
+        foreach ($orderProducts as $orderProduct) {
+            $product = $orderProduct->getProduct();
+            array_push($products, $product);
+        }
+
         return $this->render('sales_order/show.html.twig', [
             'sales_order' => $salesOrder,
+            'products' => $products
         ]);
     }
 
